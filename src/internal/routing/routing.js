47d165780c13383511c01318a0d6d906a39747c1
femiayodeji <femidotayodeji@gmail.com>
2026-07-07 01:45:00 +0100

    Clean up

--- src/internal/routing/routing.js
+++
@@ -1,8 +0,0 @@
-export const ArmyMovesPrefix = "army_moves";
-export const WarRecognitionsPrefix = "war";
-export const PauseKey = "pause";
-export const GameLogSlug = "game_logs";
-export const ExchangePerilDirect = "peril_direct";
-export const ExchangePerilTopic = "peril_topic";
-export const ExchangePerilDeadLetter = "peril_dlx";
-export const PerilDlqSlug = "peril_dlq";
